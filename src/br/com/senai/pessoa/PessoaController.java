9ca870235643338bf69a756c28b217c4c97ce15e
Larissa71 <larissakuffel@gmail.com>
2021-06-10 16:36:25 -0300

    Refatoração da controller para adicionar pessoa

--- src/br/com/senai/pessoa/PessoaController.java
+++ src/br/com/senai/pessoa/PessoaController.java
@@ -38,19 +38,51 @@ public class PessoaController {
 		System.out.print("Informe a altura: ");
 		pessoa.setAltura(tec.nextDouble());
 		
+		System.out.println("Informe o País: ");
+		tec.nextLine();
+		pessoa.setPais(tec.nextLine());
+		
+		System.out.println("Informe o Estado: ");
+		pessoa.setNomeDoEstado(tec.nextLine());
+		
+		System.out.println("Informe o nome da Cidade: ");
+		pessoa.setNomeDaCidade(tec.nextLine());
+		
+		System.out.println("Informe o nome do Bairro: ");
+		pessoa.setNomeDoBairro(tec.nextLine());
+		
+		System.out.println("Informe o Nome da Rua: ");
+		pessoa.setNomeDaRua(tec.nextLine());
+		
+		System.out.println(" Informe o Complemento: ");
+		pessoa.setComplemento(tec.nextLine());
+		
+		System.out.println("Informe o Número da Casa: ");
+		pessoa.setNumero(tec.nextLine());
+		
+		
 		return pessoa;
 	}
 		
 		public List<Pessoa> listarPessoas(List<Pessoa> pessoas){
 			
-			System.out.printf("| %20s | %4s | %5s | %6s|\n", "Nome", "Ano", "Idade", "Altura");
+			System.out.printf("| %20s | %5s | %5s | %6s| %10s | %15s | %15s | %15s | %20s | %10s | %6s\n", 
+					"Nome", "Ano", "Idade", "Altura", "Pais", "Estado", "Cidade", "Bairro","Rua", "Complemento", "Número" );
 			
 		for(int i = 0; i<pessoas.size(); i++) {
-			System.out.printf("| %20s | %6d | %5d | %6.2f", 
+			System.out.printf("| %20s | %5d | %5d | %6.2f| %10s | %15s | %15s | %15s | %20s | %11s | %6s ", 
 			pessoas.get(i).getNome(),
 			pessoas.get(i).getAnoDeNascimento(),
 			pessoas.get(i).getIdade(),
-			pessoas.get(i).getAltura());
+			pessoas.get(i).getAltura(),
+			pessoas.get(i).getPais(),
+			pessoas.get(i).getNomeDoEstado(),
+			pessoas.get(i).getNomeDaCidade(),
+			pessoas.get(i).getNomeDoBairro(),
+			pessoas.get(i).getNomeDaRua(),
+			pessoas.get(i).getComplemento(),
+			pessoas.get(i).getNumero());
+			
 		}	
 		return pessoas;
 		
